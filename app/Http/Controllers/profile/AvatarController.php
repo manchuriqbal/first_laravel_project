8f6533cafda1c2dcae38b95e1631a2dbc5abe037
manchuriqbal <QYWfiHfGiRd4L9n>
2024-05-21 01:46:48 +0600

    Storage facade

--- app/Http/Controllers/profile/AvatarController.php
+++ app/Http/Controllers/profile/AvatarController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers\profile;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UpdateAvatarRequest;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request){
 
-        $path = $request->file('avater')->store('avatars', 'public');
+        $path = Storage::disk('public')->put('avatars', $request->file('avater'));;
+
+        if ($old_avatar = $request->user()->avater) {
+            Storage::disk('public')->delete($old_avatar);
+        }
         auth()->user()->update(['avater' => $path]);
         return redirect(route('profile.edit'));
     }
